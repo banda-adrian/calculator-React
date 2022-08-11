36cecc59b9ec252fc549b3ff6ca65bce731410eb
Adrian Banda <5205bda@gmail.com>
2022-08-11 00:34:50 -0500

    Updates

---
+++ precache-manifest.7ed0711296159cc14fb134912fa9e34c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "447df0a139a1f38639bc",
+    "url": "/calculator-React/static/css/main.8d1f3185.chunk.css"
+  },
+  {
+    "revision": "447df0a139a1f38639bc",
+    "url": "/calculator-React/static/js/main.447df0a1.chunk.js"
+  },
+  {
+    "revision": "0cf5f86c9763b075b409",
+    "url": "/calculator-React/static/js/1.0cf5f86c.chunk.js"
+  },
+  {
+    "revision": "6bd9dce307c592bfd01f",
+    "url": "/calculator-React/static/js/runtime~main.6bd9dce3.js"
+  },
+  {
+    "revision": "388bd9fcbf49b4035ecfdd0fe817f4ba",
+    "url": "/calculator-React/index.html"
+  }
+];
